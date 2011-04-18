20aca32ecb53939d5ea8673761b12d3119fb6cb1
Eric Stern <firehed@gmail.com>
2011-04-18 02:10:49 -0700

    split id3v2.3 and v2.4 parsing into separate functions, general cleanup

--- model/audiofile.php
+++ model/audiofile.php
@@ -15,25 +15,11 @@ class AudioFile {
 	private $size;
 	private $startOfMusic;
 
-	function __construct($path) {
-		$this->path = $path;
-		$fh = fopen($path, 'r');
-
-		// Tag header is first 10 bytes of file
-		$header = fread($fh, 10);
-
-		$signature     = substr($header, 0 ,3);
-		$version_major = ord(substr($header, 3, 1));
-		$version_minor = ord(substr($header, 4, 1));
-		$flags         = ord(substr($header, 5, 1));
-		$this->size    = decode_synchsafe(substr($header, 6, 4));
-
-		if ($signature != 'ID3') {
-			return false;
-		}
-
-		$this->version_major = $version_major;
-		$this->version_minor = $version_minor;
+	private function parseheader() {
+		$this->version_major = ord(fread($this->fh, 1));
+		$this->version_minor = ord(fread($this->fh, 1));
+		$flags               = ord(fread($this->fh, 1));
+		$this->size          = decode_synchsafe(fread($this->fh, 4));
 
 		$this->flags['unsynchronization'] = (bool) ($flags & 0x80);
 		$this->flags['extended']          = (bool) ($flags & 0x40);
@@ -45,62 +31,71 @@ class AudioFile {
 		$this->startOfMusic = $this->size + ($this->flags['footer'] ? 20 : 10) - 1;
 
 		if ($this->flags['extended']) {
-			$this->parseExtendedHeader($fh);
+			$this->parseExtendedHeader();
 		}
+
+	}
+
+
+	function __construct($path) {
+		$this->path = $path;
+		$this->fh = fopen($path, 'r');
+
+		$signature = fread($this->fh, 3);
+		if ($signature != 'ID3') {
+			fclose($this->fh);
+			return false;
+		}
+
+		$this->parseHeader();
 
 		switch ($this->version_major) {
 			case 3:
-			case 4:
-				$this->parseTagsId3v23x($fh);
+				$this->parseTagsId3v23x();
 				break;
 			case 4:
-				echo 'ID3v2.4.x - unhandled right now';
+				$this->parseTagsId3v24x();
 				break;
+			default:
+				throw new Exception("ID3 v2.$this->version_major currently unsupported.");
 		}
 
 		if ($this->flags['footer'] && $this->version_major >= 4) {
-			$this->parseFooter($fh);
+			$this->parseFooter();
 		}
 
-		fclose($fh);
+		fclose($this->fh);
 	}
 
-	private function parseExtendedHeader($fh) {
+	private function parseExtendedHeader() {
 		throw new Exception('parseExtendedHeader not yet implemented');
 	} // parseExtendedHeader
 
-	private function parseFooter($fh) {
+	private function parseFooter() {
 		throw new Excption('parseFooter not yet implemented');
 	} // parseFooter
 
 	/*
-	 * ID3 tag frame format:
+	 * ID3v2.3.x tag frame format:
 	 * XXXXYYYYZZ....
 	 * XXXX = frame identifier
-	 * YYYY = frame content length bytes (synchsafe int or unsigned long, 
-	 * depending on version)
+	 * YYYY = frame content length bytes (unsigned long)
 	 * ZZ   = flags
 	 * .... = actual frame content
 	 */
-	function parseTagsId3v23x($fh) {
+	function parseTagsId3v23x() {
 		$filesize = filesize($this->path);
-		while (!feof($fh)) {
+		while (!feof($this->fh)) {
 			// No padding after previous frame, hit music. We're done here.
-			if (ftell($fh) >= $this->startOfMusic) {
+			if (ftell($this->fh) >= $this->startOfMusic) {
 				break;
 			}
 
-			$header = fread($fh, 10);
+			$header = fread($this->fh, 10);
 
-			$tag = substr($header, 0, 4);
-			if ($this->version_major == 3) {
-				$size = unpack('N', substr($header, 4, 4));
-				$size = $size[1];
-			}
-			else {
-				$size = decode_synchsafe(substr($header, 4, 4));
-			}
-
+			$tag   = substr($header, 0, 4);
+			$size  = unpack('N', substr($header, 4, 4));
+			$size  = $size[1];
 			$flags = unpack('n', substr($header, 8, 2));
 			$flags = $flags[1];
 
@@ -118,7 +113,52 @@ class AudioFile {
 				throw new UnexpectedValueException("Tag $tag has no size");
 			}
 			else {
-				$value = fread($fh, $size);
+				$value = fread($this->fh, $size);
+			}
+			$this->tags[] = new Tag($tag, $flags, $value);
+#			if ($tag == 'TIT2')
+#				$this->frames = new frame\TIT2($flags, $value);
+		}
+	}
+
+	/*
+	 * ID3v2.4.x tag frame format:
+	 * XXXXYYYYZZ....
+	 * XXXX = frame identifier
+	 * YYYY = frame content length bytes (synchsafe int)
+	 * ZZ   = flags
+	 * .... = actual frame content
+	 */
+	private function parseTagsId3v24x() {
+		$filesize = filesize($this->path);
+		while (!feof($this->fh)) {
+			// No padding after previous frame, hit music. We're done here.
+			if (ftell($this->fh) >= $this->startOfMusic) {
+				break;
+			}
+
+			$header = fread($this->fh, 10);
+
+			$tag   = substr($header, 0, 4);
+			$size  = decode_synchsafe(substr($header, 4, 4));
+			$flags = unpack('n', substr($header, 8, 2));
+			$flags = $flags[1];
+
+			// We've probably hit the padding leading into the music...
+			if (!trim($tag)) {
+				break;
+			}
+
+			if ($size >= $this->size) {
+				throw new Exception('Size overload ' . $size);
+			}
+			elseif (!$size) {
+				// There is something invalid with this tag - by definition 
+				// they must be at least 1 byte long
+				throw new UnexpectedValueException("Tag $tag has no size");
+			}
+			else {
+				$value = fread($this->fh, $size);
 			}
 			$this->tags[] = new Tag($tag, $flags, $value);
 #			if ($tag == 'TIT2')
