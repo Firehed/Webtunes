63d720ca636884f27ab405ff04ddc8f05c1866f5
Eric Stern <firehed@gmail.com>
2011-04-19 23:48:36 -0700

    add unofficial itunes compilation frame

---
+++ class/frame/tcmp.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace frame;
+
+class TCMP extends Frame {
+
+	protected static $description = 'iTunes - Part of Compilation';
+	protected static $is_text     = true;
+	protected static $name        = 'TCMP';
+
+}
